74e9230bd05430ff2100722b0be85dbf15eeb72f
anthonyhaffey <anthony.haffey@gmail.com>
2020-07-26 15:24:39 +0000

    Update gh-pages to output generated at ffd8430

--- kitten/Code/OnlineTriggers.js
+++ kitten/Code/OnlineTriggers.js
@@ -1,20 +1,3 @@
-
-
-/*
-
-for saving the files
-
-function google_keys(key_type){
-  switch(key_type){
-    case "researcher":
-      //resume here
-      break;
-  }
-}
-
-*/
-
-
 function online_save(experiment_id,
                      participant_id,
                      completion_code,
@@ -52,25 +35,52 @@ function online_save(experiment_id,
 			
 			console.dir("save_script_url");
 			console.dir(save_script_url);
+      console.dir("sending data");
 			
-			$.ajax({
-				type: 'POST',
-				url: save_script_url,
-				data: data,
-				crossDomain: true,
-				timeout: 120000,
-				success:function(result){
-					after_function(result);
-				}
-			})
-			.catch(function(error){
-				until_successful_script(script_list,																	
-																data,
-																after_function);
-			});
-		} else {
-			after_function();
-		}			
+      function recursive_save(save_script_url,
+                              data,
+                              attempt_no,
+                              after_function){
+        if(attempt_no == 3){
+          until_successful_script(script_list,																	
+                                  data,
+                                  after_function);
+        } else {
+          console.dir("sending data...");
+          $.ajax({
+            type: 'POST',
+            url: save_script_url, //"https://script.google.com/macros/s/AKfycbyuUWN7Jc1j62OuUh1JrJFuHn7e2VXLZdZ9FJs4dvwX_D6JI7M7/exec",
+            data: data,
+            crossDomain: true,
+            timeout: 120000,
+            success:function(result){
+              console.dir("data sending result:");
+              console.dir(result);
+              if(result == "success"){          
+                after_function();
+              } else {
+                attempt_no++;
+                console.dir("failed to save, attempting again");
+                recursive_google_save(save_script_url,
+                                      data,
+                                      attempt_no,
+                                      after_function);
+              }
+            }
+          })
+          .catch(function(error){
+            attempt_no++;
+            console.dir("failed to save, attempting again");
+            recursive_google_save(save_script_url,
+                                  data,
+                                  attempt_no,
+                                  after_function);
+          });
+        }
+      }
+      recursive_google_save(save_script_url,
+                            data,
+                            0);
 	}
 	var script_list = [];
 	Object.keys(data_scripts).forEach(function(server){
@@ -83,36 +93,3 @@ function online_save(experiment_id,
 													after_function);
 
 }
-
-
-var ParseGSX = (function() {
-
-  var _defaultCallback = function(data) {
-    console.log(data);
-  };
-
-  var _parseRawData = function(res) {
-    var finalData = [];
-    res.feed.entry.forEach(function(entry){
-      var parsedObject = {};
-      for (var key in entry) {
-        if (key.substring(0,4) === "gsx$") {
-          parsedObject[key.slice(4)] = entry[key]["$t"];
-        }
-      }
-      finalData.push(parsedObject);
-    });
-    var processGSXData = _defaultCallback;
-    processGSXData(finalData);
-  };
-
-  var parseGSX = function(spreadsheetID, callback) {
-    var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
-    var ajax = $.ajax(url);
-    if (callback) { _defaultCallback = callback; }
-    $.when(ajax).then(_parseRawData);
-  };
-
-  return { parseGSX: parseGSX };
-
-})();
