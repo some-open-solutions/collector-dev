cf56f47f5fe5014f5c65a7d4cff854ba492bcc33
anthonyhaffey <haffeylab@gmail.com>
2020-12-31 17:13:38 +0000

    open folders fixed windows side, now to check if it has been broken mac side

--- web/ElectronModules/openFolders.js
+++ web/ElectronModules/openFolders.js
@@ -8,16 +8,35 @@ const ipc  = require('electron').ipcMain;
 
 
 ipc.on('open_folder', (event,args) => {
+
+  /*
+  //detect if windows
+  if(window.navigator.platform.toLowerCase().indexOf("win") !== -1){
+    Collector.electron.open_folder("User\\Stimuli");
+
+  //detect if mac
+  } else if(window.navigator.platform.toLowerCase().indexOf("mac") !==-1 ){
+    Collector.electron.open_folder("User/Stimuli");
+
+  //assume it is linux
+  } else {
+    Collector.electron.open_folder("User/Stimuli");
+  }
+  */
+
+
   if(process.platform == "darwin"){
     var location = require("os").homedir() +
       "/Documents/Collector/" +
       args["folder"];
   } else {
-    var location = app.getAppPath() + "\\" + args["folder"];
+    var location = app.getAppPath() +
+      "\\" +
+      args["folder"].replace("/","\\");
   }
-  console.dir(location);
+  location = location.replace("resources\\app.asar\\","");
   shell.showItemInFolder(
     location
   );
-  event.returnValue = "success";
+  event.returnValue = location;
 });
